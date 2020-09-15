f646c89781c477fb1151c28e32597e18805cd6bc
Kevin Elliott <kevin@welikeinc.com>
2020-09-14 20:27:04 -0700

    libacars: add types

--- src/libacars/ffi/types.dart
+++ src/libacars/ffi/types.dart
@@ -2,4 +2,50 @@ library acars.ffi.types;
 
 import 'dart:ffi';
 
+class LaAcarsMessage extends Struct {}
+
+class LaAdscFormatterContext extends Struct {}
+
+class LaAdscTypeDescriptor extends Struct {}
+
+class LaAdscMessage extends Struct {}
+
+class LaAdscTag extends Struct {}
+
+class LaAdscNack extends Struct {}
+
+class LaAdscNonCompliantMessage extends Struct {}
+
+class LaAdscNonComplianceNotification extends Struct {}
+
+class LaAdscBasicReport extends Struct {}
+
+class LaAdscFlightId extends Struct {}
+
+class LaAdscPredictedRoute extends Struct {}
+
+class LaAdscEarthAirReference extends Struct {}
+
+class LaAdscMeteo extends Struct {}
+
+class LaAdscAirframesId extends Struct {}
+
+class LaAdscIntermediateProjection extends Struct {}
+
+class LaAdscFixedProjection extends Struct {}
+
+class LaAdscRequest extends Struct {}
+
+class LaAdscLaterialDeviationChangeEvent extends Struct {}
+
+class LaAdscReportIntervalRequest extends Struct {}
+
+class LaAdscReportingIntervalRequest extends Struct {}
+
+class LaAdscVerticalSpeedChangeEvent extends Struct {}
+
+class LaAdscAltitudeRangeChangeEvent extends Struct {}
+
+class LaAdscAircraftIntentGroupRequest extends Struct {}
+
 class LaVstring extends Struct {}
