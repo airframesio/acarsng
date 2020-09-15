17560c6fb930eda346216e9cd32c7f56b5f0525f
Kevin Elliott <kevin@welikeinc.com>
2020-09-14 23:09:45 -0700

    libacars: More signatures using the new types

--- src/libacars/ffi/signatures.dart
+++ src/libacars/ffi/signatures.dart
@@ -68,27 +68,37 @@ typedef la_format_SEQUENCE_OF_as_json = Void Function(Pointer<LaVstring> vstr, P
 
 // ### asn1-format-cpdlc
 // void la_asn1_output_cpdlc_as_text(la_vstring *vstr, asn_TYPE_descriptor_t *td, const void *sptr, int indent);
-// extern la_dict const FANSATCUplinkMsgElementId_labels[];
-// extern la_dict const FANSATCDownlinkMsgElementId_labels[];
+typedef la_asn1_output_cpdlc_as_text = Void Function(Pointer<LaVstring> vstr, Pointer<AsnTypeDescriptor> td, Pointer<Void> sptr, int indent);
 // void la_asn1_output_cpdlc_as_json(la_vstring *vstr, asn_TYPE_descriptor_t *td, const void *sptr, int indent);
+typedef la_asn1_output_cpdlc_as_json = Void Function(Pointer<LaVstring> vstr, Pointer<AsnTypeDescriptor> td, Pointer<Void> sptr, int indent);
 
 // ### asn1-util
 // int la_asn1_decode_as(asn_TYPE_descriptor_t *td, void **struct_ptr, uint8_t *buf, int size);
+typedef la_asn1_decode_as = int Function(Pointer<AsnTypeDescriptor> td, Pointer<Void> struct_ptr, Pointer<Uint8> buf, int size);
 // void la_asn1_output(la_vstring *vstr, la_asn_formatter const * const asn1_formatter_table, size_t asn1_formatter_table_len, asn_TYPE_descriptor_t *td, const void *sptr, int indent, bool const dump_unknown_types);
+typedef la_asn1_output = Void Function(Pointer<LaVstring> vstr, Pointer<LaAsnFormatter> asn1_formatter_table, int asn1_formatter_table_len, Pointer<AsnTypeDescriptor> td, Pointer<Void> sptr, int indent, bool dump_unknown_types);
 // int la_bitstream_append_msbfirst(la_bitstream_t *bs, uint8_t const *v, uint32_t const numbytes, uint32_t const numbits);
+typedef la_bitstream_append_msbfirst = int Function(Pointer<LaBitstream> bs, Pointer<Uint8> v, Uint32 numbytes, Uint32 numbits);
 // int la_bitstream_read_word_msbfirst(la_bitstream_t *bs, uint32_t *ret, uint32_t const numbits);
+typedef la_bitstream_read_word_msbfirst = int Function(Pointer<LaBitstream> bs, Pointer<Uint32> ret, Uint32 numbits);
 
 // ### bitstream
 // la_bitstream_t *la_bitstream_init(uint32_t len);
+typedef la_bitstream_init = Pointer<LaBitstream> Function(Uint32 len);
 // void la_bitstream_destroy(la_bitstream_t *bs);
+typedef la_bitstream_destroy = Void Function(Pointer<LaBitstream> bs);
 
 // ### cpdlc
 // la_proto_node *la_cpdlc_parse(uint8_t *buf, int len, la_msg_dir const msg_dir);
+typedef la_cpdlc_parse = Pointer<LaProtoNode> Function(Pointer<Uint8> buf, int len, LaMessageDirection msg_dir);
 // void la_cpdlc_format_text(la_vstring * const vstr, void const * const data, int indent);
+typedef la_cpdlc_format_text = Void Function(Pointer<LaVstring> vstr, Pointer<Void> data, int indent);
 // void la_cpdlc_format_json(la_vstring * const vstr, void const * const data);
+typedef la_cpdlc_format_json = Void Function(Pointer<LaVstring> vstr, Pointer<Void> data);
 // void la_cpdlc_destroy(void *data);
 typedef la_cpdlc_destroy = Void Function(Pointer<Void> data);
 // la_proto_node *la_proto_tree_find_cpdlc(la_proto_node *root);
+typedef la_proto_tree_find_cpdlc = Pointer<LaProtoNode> Function(Pointer<LaProtoNode> root);
 
 // ### crc
 // uint16_t la_crc16_arinc(uint8_t const *data, uint32_t len, uint16_t const crc_init);
@@ -103,9 +113,13 @@ typedef la_crc16_arinc665 = Uint16 Function(
 
 // ### hash
 // la_hash *la_hash_new(la_hash_func *compute_hash, la_hash_compare_func *compare_keys, la_hash_key_destroy_func *destroy_key, la_hash_value_destroy_func *destroy_value);
+typedef la_hash_new = Pointer<LaHash> Function(Pointer<LaHashFunc> compute_hash, Pointer<LaHashCompareFunc> compare_keys, Pointer<LaHashKeyDestroyFunc> destroy_key, Pointer<LaHashValueDestroyFunc> destroy_value);
 // bool la_hash_insert(la_hash *h, void *key, void *value);
+typedef la_hash_insert = bool Function(Pointer<LaHash> h, Pointer<Void> key, Pointer<Void> value);
 // bool la_hash_remove(la_hash *h, void *key);
+typedef la_hash_remove = bool Function(Pointer<LaHash> h, Pointer<Void> key);
 // void *la_hash_lookup(la_hash const *h, void const *key);
+typedef la_hash_lookup = Pointer<Void> Function(Pointer<LaHash> h, Pointer<Void> key);
 // uint32_t la_hash_key_str(void const *k);
 typedef la_hash_key_str = Void Function(Pointer<Void> k);
 // uint32_t la_hash_string(char const *str, uint32_t h);
@@ -116,28 +130,47 @@ typedef la_hash_compare_keys_str = bool Function(
 // void la_simple_free(void *data);
 typedef la_simple_free = Void Function(Pointer<Void> data);
 // int la_hash_foreach_remove(la_hash *h, la_hash_if_func *if_func, void *if_func_ctx);
+typedef la_hash_foreach_remove = int Function(Pointer<LaHash> h, Pointer<LaHashIfFunc> if_func, Pointer<Void> if_func_ctx);
 // void la_hash_destroy(la_hash *h);
+typedef la_hash_destroy = Void Function(Pointer<LaHash> h);
 
 // ### json
 // void la_json_object_start(la_vstring * const vstr, char const * const key);
+typedef la_json_object_start = Void Function(Pointer<LaVstring> vstr, Pointer<Utf8> key);
 // void la_json_object_end(la_vstring * const vstr);
+typedef la_json_object_end = Void Function(Pointer<LaVstring> vstr);
 // void la_json_array_start(la_vstring * const vstr, char const * const key);
+typedef la_json_array_start = Void Function(Pointer<LaVstring> vstr, Pointer<Utf8> key);
 // void la_json_array_end(la_vstring * const vstr);
+typedef la_json_array_end = Void Function(Pointer<LaVstring> vstr);
 // void la_json_append_bool(la_vstring * const vstr, char const * const key, bool const val);
+typedef la_json_append_bool = Void Function(Pointer<LaVstring> vstr, Pointer<Utf8> key, bool val);
 // void la_json_append_double(la_vstring * const vstr, char const * const key, double const val);
+typedef la_json_append_double = Void Function(Pointer<LaVstring> vstr, Pointer<Utf8> key, double val);
 // void la_json_append_long(la_vstring * const vstr, char const * const key, long const val);
+typedef la_json_append_long = Void Function(Pointer<LaVstring> vstr, Pointer<Utf8> key, ...);
 // void la_json_append_char(la_vstring * const vstr, char const * const key, char const  val);
+typedef la_json_append_char = Void Function(Pointer<LaVstring> vstr, Pointer<Utf8> key, String val);
 // void la_json_append_string(la_vstring * const vstr, char const * const key, char const * const val);
+typedef la_json_append_string = Void Function(Pointer<LaVstring> vstr, Pointer<Utf8> key, Pointer<Utf8> val);
 // void la_json_append_octet_string(la_vstring * const vstr, char const * const key, uint8_t const * const buf, size_t len);
+typedef la_json_append_octet_string = Void Function(Pointer<LaVstring> vstr, Pointer<Utf8> key, Pointer<Uint8> buf, int len);
 // void la_json_start(la_vstring * const vstr);
+typedef la_json_start = Void Function(Pointer<LaVstring> vstr);
 // void la_json_end(la_vstring * const vstr);
+typedef la_json_end = Void Function(Pointer<LaVstring> vstr);
 
 // ### libacars
 // la_proto_node *la_proto_node_new();
+typedef la_proto_node_new = Pointer<LaProtoNode> Function();
 // la_vstring *la_proto_tree_format_text(la_vstring *vstr, la_proto_node const * const root);
+typedef la_proto_tree_format_text = Pointer<LaVstring> Function(Pointer<LaVstring> vstr, Pointer<LaProtoNode> root);
 // la_vstring *la_proto_tree_format_json(la_vstring *vstr, la_proto_node const * const root);
+typedef la_proto_tree_format_json = Pointer<LaVstring> Function(Pointer<LaVstring> vstr, Pointer<LaProtoNode> root);
 // void la_proto_tree_destroy(la_proto_node *root);
+typedef la_proto_tree_destroy = Void Function(Pointer<LaProtoNode> root);
 // la_proto_node *la_proto_tree_find_protocol(la_proto_node *root, la_type_descriptor const * const td);
+typedef la_proto_tree_find_protocol = Pointer<LaProtoNode> Function(Pointer<LaProtoNode> root, Pointer<LaTypeDescriptor> td);
 // void la_config_init();
 typedef la_config_init = Void Function();
 // void la_config_destroy();
