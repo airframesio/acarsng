ad28b7e6c8cf4fc7fc1b3be848eb387e2a48c0b1
Kevin Elliott <kevin@welikeinc.com>
2020-09-14 18:50:33 -0700

    Get devices and loop over them

--- src/acarsng.dart
+++ src/acarsng.dart
@@ -1,1 +1,14 @@
 library acarsng;
+
+import 'librtlsdr/rtlsdr.dart';
+
+void main() {
+  getDevices().forEach((device) {
+    print('Device: ${device}');
+
+    final deviceStrings = device.deviceStrings;
+    print('Manufacturer: ${deviceStrings.manufacturerName}');
+    print('Product: ${deviceStrings.productName}');
+    print('Serial: ${deviceStrings.serialNumber}');
+  });
+}
